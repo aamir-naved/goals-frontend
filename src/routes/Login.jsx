1fa81159fbc89b8ecd05d5e2f034b147f5713ef9
aamirnaved <aamirnaved0020@gmail.com>
2025-03-06 10:57:20 +0530

    fixing login2

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { loginUser } from "../api/auth"; 
 import { useNavigate } from "react-router-dom";
 import Loader from "./Loader";
